5741ce7458af5870d952787606fb6a6988ea47d1
seavmeyngat <seavmey.ngat@institute.pse.ngo>
2025-03-03 16:39:59 +0700

    update

---
+++ src/pages/WorkoutPage.tsx
@@ -0,0 +1,11 @@
+
+
+const WorkoutPage = () => {
+  return (
+    <div>
+      Workout Page
+    </div>
+  )
+}
+
+export default WorkoutPage
--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -2,6 +2,8 @@ import { JSX } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import ProfilePage from "../pages/ProfilePage";
 import Layout from "../pages/Layout";
+import WorkoutPage from "../pages/WorkoutPage";
+import WorkoutPlanApp from "../pages/workout";
 
 export interface AppRoute {
   path?: string;
@@ -12,6 +14,7 @@ export interface AppRoute {
 
 export const webRoutes = {
   home: "/",
+  workoutPage: "/workout-page"
   // discover: "/discover",
 };
 
@@ -27,6 +30,7 @@ const appRoutes: AppRoute[] = [
     element: <Layout />,
     children: [
       { path: webRoutes.home, element: <ProfilePage /> },
+      { path: webRoutes.workoutPage, element: <WorkoutPlanApp /> },
       // { path: webRoutes.discover, element: <DiscoverPage /> },
     ],
   },
